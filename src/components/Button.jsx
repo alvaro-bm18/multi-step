ee53cfd30aaa47c72de22ea065b4ed9c0802a513
= <=>
2023-03-05 17:55:35 -0600

    frontend mentor

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import '../styles/Button.css';
+
+function Button(props){
+    const setText = (type) => {
+        if(type === 'go-back') return 'Go Back';
+        else if(type === 'go-next') return 'Next';
+        else return 'Confirm';
+    }
+    return (
+        <div
+            className={`button ${props.type}`}
+            onClick={props.action}>
+            {setText(props.type)}
+        </div>
+    );
+}
+
+export default Button;
---
+++ src/components/HeaderForm.jsx
@@ -0,0 +1,12 @@
+import '../styles/HeaderForm.css';
+
+function HeaderForm(props){
+    return (
+        <header className='header-section'>
+            <strong>{props.title}</strong>
+            <span>{props.desc}</span>
+        </header>
+    );
+}
+
+export default HeaderForm;
